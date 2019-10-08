8d386a6ae4f78386268d731989638eb364818362
Alexandre Tolstenko <tolstenko@gmail.com>
2019-10-08 09:16:18 -0300

    feat(snake-mechanic): initial objects

---
+++ Assets/Scenes/SnakeControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeControl : MonoBehaviour
+{
+    private SnakeModel m_Model;
+    public SnakeBlock BlockReference;
+
+    public void Start()
+    {
+        // find the model
+        m_Model = FindObjectOfType<SnakeModel>();
+        
+        // create the 3 initial blocks
+        SnakeBlock obj = Instantiate(BlockReference, Vector3.zero, Quaternion.identity);
+        m_Model.SnakeBlocks.Add(obj);
+        
+        obj = Instantiate(BlockReference, 
+            new Vector3(-1,0,0), Quaternion.identity);
+        m_Model.SnakeBlocks.Add(obj);
+        
+        obj = Instantiate(BlockReference, 
+            new Vector3(-2,0,0), Quaternion.identity);
+        m_Model.SnakeBlocks.Add(obj);
+    }
+}
---
+++ Assets/Scenes/SnakeModel.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeModel : MonoBehaviour
+{
+    public List<SnakeBlock> SnakeBlocks;
+}
